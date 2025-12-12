5e379aef46b58f00a4cff01479b81d467cbc03d4
RahimOnGit <semo@Semos-MacBook-Pro.local>
2025-12-12 11:57:18 +0100

    project set up

---
+++ src/main/java/com/example/fbk_balkan/FbkBalkanApplication.java
@@ -0,0 +1,13 @@
+package com.example.fbk_balkan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbkBalkanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FbkBalkanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fbk_balkan/FbkBalkanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fbk_balkan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbkBalkanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
