8726e20ab7a3fd54558808e69add08050e1393d1
RahimOnGit <abdalraheem.saif7777@gmail.com>
2026-01-12 20:58:37 +0100

    merged with about page

--- src/main/java/com/example/fbk_balkan/security/SecurityConfig.java
+++ src/main/java/com/example/fbk_balkan/security/SecurityConfig.java
@@ -58,6 +58,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers("/", "/login", "/register", "/dashboard", "/css/**", "/images/**", "/js/**").permitAll()
+                        .requestMatchers("/about").permitAll()
                         .requestMatchers("/api/dashboard").authenticated()
                         .anyRequest().authenticated()
                 )
