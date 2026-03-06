33ad4340ba79a3435591b7ae09d696f38e67b229
hadia55op <hadizaidkhan@gmail.com>
2026-03-06 10:22:37 +0100

    FAQ

---
+++ src/main/java/com/example/fbk_balkan/controller/FaqController.java
@@ -0,0 +1,16 @@
+//package com.example.fbk_balkan.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class FaqController {
+//    @GetMapping("/faq")
+//    public String fag(){
+//        return "faq";
+//
+//    }
+//
+//
+//
+//}
