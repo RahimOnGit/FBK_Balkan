cf778d3c6ea847602620b53dab368b6564d25ef7
RahimOnGit <abdalraheem.saif7777@gmail.com>
2026-04-09 13:04:50 +0200

    match results and upcoming matches from API to our db , new entity match

--- src/main/java/com/example/fbk_balkan/controller/HomeController.java
+++ src/main/java/com/example/fbk_balkan/controller/HomeController.java
@@ -39,8 +39,11 @@
 
 package com.example.fbk_balkan.controller;
 
+import com.example.fbk_balkan.dto.match.GameDTO;
 import com.example.fbk_balkan.entity.News;
 import com.example.fbk_balkan.repository.FaqRepository;
+import com.example.fbk_balkan.repository.MatchRepository;
+import com.example.fbk_balkan.service.MatchService;
 import com.example.fbk_balkan.service.NewsService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -53,21 +56,29 @@ public class HomeController {
 
     private final NewsService newsService;
     private final FaqRepository faqRepository;
+    private final MatchService matchService;
 
-    public HomeController(NewsService newsService,FaqRepository faqRepository) {
+    public HomeController(NewsService newsService, FaqRepository faqRepository, MatchRepository matchRepository, MatchService matchService) {
         this.newsService = newsService;
         this.faqRepository = faqRepository;
+        this.matchService = matchService;
     }
 
     @GetMapping("/")
     public String home(Model model) {
         List<News> latestNews = newsService.getAllPublishedNews();
         News latestSingleNews = latestNews.isEmpty() ? null : latestNews.get(0);
+//      fetch last news
         model.addAttribute("latestNews", latestSingleNews);
+//     fetch   FAQ
         model.addAttribute(
                 "homeFaqs",
                 faqRepository.findTop3ByVisibleTrueOrderByDisplayOrderAsc()
         );
+//      fetch Match results
+        List<GameDTO> matches = matchService.fetchMatches();
+        model.addAttribute("matches" , matches);
+        System.out.println("NEW Match results: " + matchService.fetchMatches());
         return "index";
     }
 
