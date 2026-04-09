c4f29a46aa72d2bae56fd10b83ec8c8b325efd73
Khaled Ibrahim <khaled.abdo11@gmail.com>
2026-04-09 12:43:30 +0200

    Create Kläder page

--- src/main/java/com/example/fbk_balkan/enums/SponsorCategory.java
+++ src/main/java/com/example/fbk_balkan/enums/SponsorCategory.java
@@ -1,11 +1,19 @@
 package com.example.fbk_balkan.enums;
 
 public enum SponsorCategory {
-    MAIN("Huvudsponsor"),
-    GOLD("Guldsponsor"),
-    SILVER("Silversponsor"),
-    BRONZE("Bronsponsor"),
-    PARENT("Föräldersponsor");
+//    MAIN("Huvudsponsor"),
+//    GOLD("Guldsponsor"),
+//    SILVER("Silversponsor"),
+//    BRONZE("Bronsponsor"),
+//    PARENT("Föräldersponsor");
+
+MAIN("Huvudpartner"),
+    GOLD("Företagspartners"),
+    SILVER("Samarbetspartners"),
+    SPRING_BALL("Vårens Bollpartner"),
+    AUTUMN_BALL("Höstens Bollpartner"),
+    BRONZE("Stödpartners"),
+    PARENT("Barnens Partner");
 
     private final String displayName;
 
